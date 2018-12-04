d86b98d54acbfa1402a244b37b07c6de07cfdb47
Kostas Sotiropoulos <kosotiro@yahoo.gr>
2018-12-04 02:52:31 +0200

    CaserShiftCipher updated

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,21 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
 public class CaesarShiftCipherTest {
+
+    @ParameterizedTest(name="str {0},cipherResult {1}")
+    @CsvSource({ "abc,def"})
+    public void testCaesarCipher(String str,String cipherstr){
+
+        String cipherResult = new CaesarShiftCipher().CaesarShiftCipher(str,3);
+
+        //Assertions.assertTrue(expectedResult == result);
+        Assertions.assertEquals(cipherstr, cipherResult);
+
+    }
 
 }
